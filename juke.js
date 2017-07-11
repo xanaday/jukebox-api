de91e92cbe7261e3d64c498b2a190db289456f16
xanaday <kate.kolbell@gmail.com>
2017-07-11 09:34:01 -0400

    soundcloud

--- juke.js
+++ juke.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   console.log('js attached');
   $('#pause').prop('disabled', true);
 
-  // SC.initialize({
-  //   client_id: 'fd4e76fc67798bfa742089ed619084a6';
-  // })
-  //
+  SC.initialize({
+    client_id: 'fd4e76fc67798bfa742089ed619084a6'
+  });
+
   // var track_url = 'http://soundcloud.com/forss/flickermood';
   // SC.oEmbed(track_url, {
   //   auto_play: true
@@ -13,17 +13,63 @@ $(document).ready(function() {
   //   console.log('oEmbed response: ', oEmbed);
   // });
 
-function Jukebox() {
-  this.playlist = ['song1.mp3', 'song2.mp3', 'song3.mp3', 'song4.mp3']
-  this.currentIndex = 0
-  this.audio = $('#audioPlayer')[0]
-  // console.log(this.audio)
+  function Jukebox(audio) {
+    var _this = this;
+    this.player = null;
+    this.audio = audio;
+    this.songs = ['https://bit.ly/2sXBeXS', 'https://bit.ly/2tvIFmG'];
+    this.currentSongIndex = 0;
+    this.currentSongID = 0;
 
-  this.play = function(){
-      this.audio.src = this.playlist[this.currentIndex]
-      this.audio.play();
-      // console.log(this.currentIndex)
-  }
+    this.renderSong = function(song) {
+      var $songTitleElem = document.getElementById('song-title');
+      $songTitleElem.innerHTML = song.title;
+    }
+
+    this.renderTracks = function(list) {
+      var _this = this;
+      var display = document.getElementById('display');
+      display.innerHTML = '';
+
+      var i = 0;
+      list.forEach(function(item) {
+        var $item = document.createElement('div');
+        $item.innerHTML = item.title;
+        $item.classList.add('track');
+        $item.setAttribute('id', item.id);
+        $item.setAttribute('index', i);
+        $item.addEventListener('click', function(){
+          _this.currentSongIndex =  $item.getAttribute('index');
+          _this.play();
+        })
+        display.appendChild($item);
+        i++;
+      })
+    }
+
+    this.search = function(query) {
+      SC.get('/tracks', {
+        q: query
+      }).then(function(tracks) {
+        this.songs = tracks.splice(0, 10);
+        this.renderTracks(this.songs);
+      }.bind(this));
+    }
+
+    this.play = function() {
+      var song = this.songs[this.currentSongIndex];
+
+      if(this.currentSongID !== song.id) {
+        this.currentSongID = song.id;
+        this.audio = SC.stream('/tracks/' + song.id)
+      }
+
+      this.audio.then(function(player){
+        this.player = player;
+        this.renderSong(song);
+        this.player.play();
+      }.bind(this));
+    }
 
   this.pause = function(){
       this.audio.pause();
@@ -73,9 +119,9 @@ $('#prevBtn').click(function(){
 $('#nextBtn').click(function(){
   thisIsMyJukebox.next()
 })
-$('li').click(function(event){
-  thisIsMyJukebox.currentIndex = event.target.id
-  thisIsMyJukebox.pause();
-  thisIsMyJukebox.play();
-})
+input.addEventListener('keyup', function() {
+  myJukebox.search(input.value);
+});
+
+myJukebox.search('');})
 });
