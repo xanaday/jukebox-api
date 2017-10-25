df1e2f6f3611be79b268ec6d3508335e4d883060
xanaday <dreamwalker.19@gmail.com>
2017-10-24 23:17:15 -0400

    updated

--- juke.js
+++ juke.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
   $('#pause').prop('disabled', true);
 
   SC.initialize({
-    client_id: 'fd4e76fc67798bfa742089ed619084a6'
+    client_id: 'fd4e76fc67798bfa742089ed619084a6',
+    redirect_uri: './callback.html'
   });
+
+  //SC.initialize({
+  //  client_id: 'fd4e76fc67798bfa742089ed619084a6'
+  //});
 
   // var track_url = 'http://soundcloud.com/forss/flickermood';
   // SC.oEmbed(track_url, {
@@ -16,7 +21,7 @@ $(document).ready(function() {
   function Jukebox(audio) {
     var _this = this;
     this.player = null;
-    this.audio = audio;
+    this.audio = audio
     this.songs = ['https://bit.ly/2sXBeXS', 'https://bit.ly/2tvIFmG'];
     this.currentSongIndex = 0;
     this.currentSongID = 0;
@@ -47,6 +52,21 @@ $(document).ready(function() {
       })
     }
 
+    <script src="https://connect.soundcloud.com/sdk/sdk-3.2.2.js"></script>
+    <script>
+    SC.initialize({
+      client_id: 'fd4e76fc67798bfa742089ed619084a6'
+    });
+    
+     {/* find all sounds of buskers licensed under 'creative commons share alike' */}
+    SC.get('/tracks', {
+      q: 'buskers', license: 'cc-by-sa'
+    }).then(function(tracks) {
+      console.log(tracks);
+    });
+    </script>
+
+    /*
     this.search = function(){
       console.log("clicked here")
       var userInput = $('#q').val()
@@ -62,58 +82,65 @@ $(document).ready(function() {
       });
       // };
     }
+    */
 
     this.play = function() {
       var song = this.songs[this.currentSongIndex];
-      if(this.currentSongID !== song.id) {
-        this.currentSongID = song.id;
-        let self = this;
-        SC.stream('/tracks/' + song.id).then(function(player){
-          self.audio = player;
-          player.play()
-          console.log(self);
+      this.audio.src = song;
+      this.audio.play();
+     // if(this.currentSongID !== song.id) {
+     //   this.currentSongID = song.id;
+       // let self = this;
+
+       // SC.stream('/tracks/' + song.id).then(function(player){
+       //   self.audio = player;
+       //   player.play()
+       //   console.log(self);
           // when you call this.rendersong, there's an error inside of it.
           // that error stops the javascript from continuing past this function.
           // we had player.play after this. That's all.
-          self.renderSong(song);
-        });
-      }
+       //   self.renderSong(song);
+       // });
+     // }
     }
 
   this.pause = function(){
       this.audio.pause();
-      console.log(this);
-
   }
 
   this.stop = function(){
       this.audio.pause();
-      console.log(this);
       this.audio.currentTime = 0;
   }
 
   this.next = function(){
-      this.audio.pause();
-      this.currentIndex++;
-      if(this.currentIndex == this.songs.length){
-        this.currentIndex = 0
-      };
-      this.audio.src = this.songs[this.currentIndex];
-      this.play();
+
+      if(this.currentSongIndex === (this.songs.length - 1)){
+        this.currentSongIndex = 0
+        this.audio.src = this.songs[this.currentSongIndex];
+        this.audio.play();
+      } else {
+        this.currentSongIndex++;
+        this.audio.src = this.songs[this.currentSongIndex];
+        this.audio.play();
+      }
   }
 
   this.prev = function(){
-      this.audio.pause();
-      this.currentIndex--;
-      if(this.currentIndex <= 0){
-        this.currentIndex = this.playlist.length - 1
-      };
-      this.audio.src = this.playlist[this.currentIndex];
-      this.play();
+      if(this.currentSongIndex <= 0){
+        this.currentSongIndex = this.songs.length - 1
+        this.audio.src = this.songs[this.currentSongIndex];
+        this.play();
+
+      } else {
+        this.currentSongIndex--;
+        this.audio.src = this.songs[this.currentSongIndex];
+        this.play();
+      }
   }
 }
-
-var thisIsMyJukebox = new Jukebox()
+var audioTag = document.getElementById("audioPlayer");
+var thisIsMyJukebox = new Jukebox(audioTag);
 
 //pass the play function by reference
 $('#playBtn').click(function(){
